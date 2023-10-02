956f77111ad393595b2aaff16e2d508095ee7d4e
konstantin.litvin <example@gmail.com>
2023-10-02 09:45:22 +0300

    rebase produce method for producer

---
+++ internal/kafka/client.go
@@ -0,0 +1,102 @@
+package kafka
+
+import (
+	"context"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"kafka_client/internal/entity"
+	"kafka_client/pkg/logger"
+	"time"
+)
+
+const (
+	// Максимальное количество реплик каждой партиции (равно количеству брокеров в кластере)
+	maxReplicationFactor = 3
+
+	// Значение реплик каждой партиции по умолчанию
+	defaultReplicationFactor = 1
+	// Значение партиций для топика по умолчанию
+	defaultNumPartitions = 3
+
+	// Время ожидания, пока очередь в буфере продусера переполнена
+	queueFullWaitTime = time.Second * 5
+
+	reconnectTime = time.Second * 10
+
+	flushTimeout = 5000
+
+	readTimeout = time.Second
+)
+
+type client struct {
+	serviceName string
+	topicPrefix string
+	consumers   consumers
+	producer    *producer
+}
+
+func NewBrokerClient(
+	producerConfig kafka.ConfigMap,
+	consumerConfig kafka.ConfigMap,
+	serviceName string,
+) entity.BrokerClient {
+	consumerConfig["group.id"] = serviceName
+	return &client{
+		serviceName: serviceName,
+		producer:    newProducer(producerConfig),
+		consumers:   newConsumers(consumerConfig),
+	}
+}
+
+func Start(client entity.BrokerClient) {
+	log := logger.GetLogger()
+	log.Info("START CONNECTING TO KAFKA")
+	err := client.Start()
+	if err != nil {
+		log.Fatal(err, "can't start kafka client")
+	}
+}
+
+func (c *client) Start() (err error) {
+	err = c.producer.initProducer()
+	if err != nil {
+		return
+	}
+	if len(c.consumers.consumers) != 0 {
+		err = c.producer.createTopics(c.consumers.getUniqByNameTopicSpecifications())
+		if err != nil {
+			return
+		}
+	}
+	c.consumers.initConsumers()
+	return
+}
+
+func (c *client) Pre(mw ...entity.MiddlewareFunc) {
+	for _, v := range mw {
+		c.consumers.mwFuncs = append(c.consumers.mwFuncs, v)
+	}
+}
+
+func (c *client) StopSubscribe() {
+	c.consumers.stopConsumers()
+}
+
+func (c *client) StopProduce() {
+	c.producer.stop()
+}
+
+func (c *client) Publish(ctx context.Context, message entity.Message) (err error) {
+	message.Topic = c.topicPrefix + message.Topic
+	return c.producer.publish(ctx, message)
+}
+
+func (c *client) Subscribe(h entity.Handler, countConsumers int, spec entity.TopicSpecifications) {
+	log := logger.GetLogger()
+	spec.Topic = c.topicPrefix + spec.Topic
+	for j := 0; j < countConsumers; j++ {
+		err := c.consumers.addNewConsumer(h, spec)
+		if err != nil {
+			log.Fatal(err, "can't create new consumer")
+		}
+	}
+}
---
+++ internal/kafka/producer.go
@@ -0,0 +1,150 @@
+package kafka
+
+import (
+	"context"
+	"github.com/CossackPyra/pyraconv"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	"kafka_client/internal/entity"
+	"kafka_client/pkg/logger"
+	"time"
+)
+
+type producer struct {
+	config        kafka.ConfigMap
+	kafkaProducer *kafka.Producer
+}
+
+func newProducer(config kafka.ConfigMap) *producer {
+	return &producer{
+		config: config,
+	}
+}
+
+func (p *producer) initProducer() (err error) {
+	log := logger.GetLogger()
+	p.config["client.id"] = uuid.New().String()
+
+	// FIXME Два костыля, нужно подумать, что делать с тем, что с консула числа маршлятся во float64
+	p.config["queue.buffering.max.messages"] = int(pyraconv.ToInt64(p.config["queue.buffering.max.messages"]))
+	p.config["linger.ms"] = int(pyraconv.ToInt64(p.config["linger.ms"]))
+
+	p.kafkaProducer, err = kafka.NewProducer(&p.config)
+	if err != nil {
+		return errors.Wrap(err, "cant create kafka producer")
+	}
+	log.Info("KAFKA PRODUCER IS READY")
+	return nil
+}
+
+func (p *producer) stop() {
+	p.kafkaProducer.Flush(flushTimeout)
+	p.kafkaProducer.Close()
+}
+
+func (p *producer) produce(ctx context.Context, message *kafka.Message, deliveryChannel chan kafka.Event) error {
+	log := logger.FromContext(ctx)
+	for {
+		err := p.kafkaProducer.Produce(message, deliveryChannel)
+		if err != nil {
+			if err.(kafka.Error).Code() == kafka.ErrQueueFull {
+				// Если очередь забита, пробуем отправить снова через 5 секунд
+				log.WithError(err).
+					Warnf("kafka queue full, try again after %v second", queueFullWaitTime.Seconds())
+				time.Sleep(queueFullWaitTime)
+				continue
+			} else {
+				return err
+			}
+		}
+		break
+	}
+	return nil
+}
+
+func (p *producer) createTopics(topics []entity.TopicSpecifications) (err error) {
+	// Создаём админский клиент через настройки подключения продусера
+	adminClient, err := kafka.NewAdminClientFromProducer(p.kafkaProducer)
+	if err != nil {
+		return errors.Wrap(err, "cant init kafka admin client")
+	}
+	defer adminClient.Close()
+	log := logger.GetLogger()
+	specifications := make([]kafka.TopicSpecification, 0, len(topics))
+	for _, topic := range topics {
+		specification := kafka.TopicSpecification{
+			Topic:             topic.Topic,
+			ReplicationFactor: defaultReplicationFactor,
+			NumPartitions:     defaultNumPartitions,
+		}
+		// Если нет настроек топика, то при создании будут подставляться дефолтные
+		if topic.ReplicationFactor != 0 {
+			if topic.ReplicationFactor > maxReplicationFactor {
+				topic.ReplicationFactor = maxReplicationFactor
+			}
+			specification.ReplicationFactor = topic.ReplicationFactor
+		}
+		if topic.NumPartitions > 0 {
+			specification.NumPartitions = topic.NumPartitions
+		}
+		specifications = append(specifications, specification)
+	}
+	result, err := adminClient.CreateTopics(context.Background(), specifications)
+	if err != nil {
+		return errors.Wrapf(err, "%v: cant create topics", err.Error())
+	}
+	for _, v := range result {
+		// Если такой топик уже есть, то будет ошибка внутри структуры, если ошибки нет, то в структуре будет "Success"
+		log.Infof("%v: %v", v.Topic, v.Error.String())
+	}
+	return nil
+}
+
+func (p *producer) publish(ctx context.Context, message entity.Message) (err error) {
+	deliveryChannel := make(chan kafka.Event)
+
+	go p.handleDelivery(ctx, message, deliveryChannel)
+
+	err = p.produce(
+		ctx,
+		message.ToKafkaMessage(),
+		deliveryChannel,
+	)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+func (p *producer) handleDelivery(ctx context.Context, message entity.Message, deliveryChannel chan kafka.Event) {
+	log := logger.FromContext(ctx)
+	e := <-deliveryChannel
+	close(deliveryChannel)
+	switch event := e.(type) {
+	case *kafka.Message:
+		if event.TopicPartition.Error != nil {
+			kafkaErr := event.TopicPartition.Error.(kafka.Error)
+			// Если retriable, то ошибка временная, нужно пытаться переотправить снова, если нет, то ошибка nonretriable, просто логируем
+			if kafkaErr.IsRetriable() {
+				log.WithError(kafkaErr).
+					Errorf("kafka produce retriable error, try again send topic: %v, message: %v",
+						message.Topic, string(message.Body))
+				err := p.publish(ctx, message)
+				if err != nil {
+					log.WithError(err).
+						Errorf("Cant publish by kafka, topic: %v, message: %v",
+							message.Topic, string(message.Body))
+				}
+			} else {
+				log.WithError(kafkaErr).
+					Errorf("kafka produce nonretriable error, can't send topic: %v, message: %v. Is fatal: %v",
+						message.Topic, string(message.Body), kafkaErr.IsFatal())
+			}
+		}
+	case kafka.Error:
+		// Общие пользовательские ошибки, клиент сам пытается переотправить, просто логируем
+		log.WithError(event).
+			Errorf("publish error, topic: %v, message: %v. client tries to send again", message.Topic, string(message.Body))
+	}
+}

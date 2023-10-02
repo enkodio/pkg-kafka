b240281f4799cdd7f5203ea2b9bb9605b05a7844
konstantin.litvin <example@gmail.com>
2023-10-02 09:46:19 +0300

    clear

---
+++ internal/entity/kafka.go
@@ -0,0 +1,28 @@
+package entity
+
+import (
+	"context"
+)
+
+type (
+	MessageHandler func(ctx context.Context, message Message) error
+	MiddlewareFunc func(next MessageHandler) MessageHandler
+)
+
+type Handler func(ctx context.Context, message []byte) error
+
+type TopicSpecifications struct {
+	Topic             string
+	NumPartitions     int
+	ReplicationFactor int
+	CheckError        bool
+}
+
+type BrokerClient interface {
+	Start() error
+	Pre(mw ...MiddlewareFunc)
+	StopSubscribe()
+	StopProduce()
+	Publish(context.Context, Message) error
+	Subscribe(Handler, int, TopicSpecifications)
+}

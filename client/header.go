3e047eac8b0be5fa374cf89734d621432bfd0a3b
konstantin.litvin <example@gmail.com>
2023-10-12 10:24:37 +0300

    fix headers

--- client/header.go
+++ client/header.go
@@ -1,9 +1,5 @@
 package client
 
-import (
-	"github.com/confluentinc/confluent-kafka-go/kafka"
-)
-
 const (
 	serviceNameHeaderKey = "service_name"
 )
@@ -13,46 +9,7 @@ type Header interface {
 	GetValue() []byte
 }
 
-type MessageHeader struct {
-	Key   string `json:"key"`
-	Value []byte `json:"value"`
-}
-
-func NewByKafkaHeaders(kafkaHeaders []kafka.Header) Headers {
-	var headers = make([]Header, len(kafkaHeaders))
-	for i := 0; i < len(kafkaHeaders); i++ {
-		headers[i] = &MessageHeader{
-			Key:   kafkaHeaders[i].Key,
-			Value: kafkaHeaders[i].Value,
-		}
-	}
-	return headers
-}
-
-func (m MessageHeader) GetKey() string {
-	return m.Key
-}
-
-func (m MessageHeader) GetValue() []byte {
-	return m.Value
-}
-
 type Headers []Header
-
-func (h Headers) toKafkaHeaders() []kafka.Header {
-	var headers = make([]kafka.Header, len(h))
-	for i, header := range h {
-		headers[i] = kafka.Header{
-			Key:   header.GetKey(),
-			Value: header.GetValue(),
-		}
-	}
-	return headers
-}
-
-func (h *Headers) SetServiceName(serviceName string) {
-	h.SetHeader(serviceNameHeaderKey, []byte(serviceName))
-}
 
 func (h Headers) GetValueByKey(key string) []byte {
 	for _, header := range h {
@@ -68,19 +25,4 @@ func (h *Headers) SetHeader(key string, value []byte) {
 		Key:   key,
 		Value: value,
 	})
-}
-
-func (h Headers) GetValidHeaders() Headers {
-	validHeaders := make([]Header, 0, len(h))
-	for _, header := range h {
-		if header == nil {
-			continue
-		}
-		validHeaders = append(validHeaders, header)
-	}
-	return validHeaders
-}
-
-func (h Headers) GetServiceName() string {
-	return string(h.GetValueByKey(serviceNameHeaderKey))
 }
